8fc83e87dbf93aeb12856c6d2c41f8b7d8812591
Scribble <ScribbleLP@users.noreply.github.com>
2020-12-14 21:50:58 +0100

    Fixed compiler warnings, caused by refmap

--- src/main/java/de/scribble/lp/tasmod/mixin/MixinGameSettings.java
+++ src/main/java/de/scribble/lp/tasmod/mixin/MixinGameSettings.java
@@ -11,11 +11,11 @@ import net.minecraft.client.settings.KeyBinding;
 @Mixin(GameSettings.class)
 public class MixinGameSettings {
 	
-	@Redirect(method = "isKeyDown", at = @At(value= "INVOKE", target = "Lorg/lwjgl/input/Mouse;isButtonDown(I)Z",ordinal = 0))
+	@Redirect(method = "isKeyDown", at = @At(value= "INVOKE", target = "Lorg/lwjgl/input/Mouse;isButtonDown(I)Z",ordinal = 0), remap = false)
 	private static boolean redirectIsKeyDown1(int i, KeyBinding key) {
 		return VirtualMouseAndKeyboard.isKeyDown(i+100);
 	}
-	@Redirect(method = "isKeyDown", at = @At(value= "INVOKE", target = "Lorg/lwjgl/input/Keyboard;isKeyDown(I)Z",ordinal = 0))
+	@Redirect(method = "isKeyDown", at = @At(value= "INVOKE", target = "Lorg/lwjgl/input/Keyboard;isKeyDown(I)Z",ordinal = 0), remap = false)
 	private static boolean redirectIsKeyDown2(int i, KeyBinding key) {
 		return VirtualMouseAndKeyboard.isKeyDown(i);
 	}
