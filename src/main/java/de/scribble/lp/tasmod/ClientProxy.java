ec1dd49949bdcfd5457e471dee3bfa20e5e2439c
Scribble <ScribbleLP@users.noreply.github.com>
2020-12-17 00:09:02 +0100

    Added ingame tutorial
    Closed #11
    Ready do realease the first alpha version I guess?

--- src/main/java/de/scribble/lp/tasmod/ClientProxy.java
+++ src/main/java/de/scribble/lp/tasmod/ClientProxy.java
@@ -1,20 +1,34 @@
 package de.scribble.lp.tasmod;
 
+import de.scribble.lp.tasmod.tutorial.TutorialHandler;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends CommonProxy{
 	
+	public static Configuration config;
+	
+	static TutorialHandler playbackTutorial;
+	
 	public void preInit(FMLPreInitializationEvent ev) {
+		config = new Configuration(ev.getSuggestedConfigurationFile());
+		Config.reloadClientConfig(config);
 		super.preInit(ev);
 	}
 	public void init(FMLInitializationEvent ev) {
+		playbackTutorial=new TutorialHandler((short)1);
 		MinecraftForge.EVENT_BUS.register(new InfoGui());
+		MinecraftForge.EVENT_BUS.register(playbackTutorial);
+		
 		super.init(ev);
 	}
 	public void postInit(FMLPostInitializationEvent ev) {
 		super.postInit(ev);
+	}
+	public static TutorialHandler getPlaybackTutorial() {
+		return playbackTutorial;
 	}
 }
