066acdb96ddf0e6ccecb835f70f6e4469404d101
Scribble <ScribbleLP@users.noreply.github.com>
2021-01-18 01:01:26 +0100

    Added file copying to SavestateHandler
    -Moved Exceptions to a new package
    -Created Loadstate exception
    -Added a check to tickratechanger client and server for tickrate saved to stop a softlock
    -Added the Savestate guiscreen
    -More documentation to the savestate packet

--- src/main/java/de/scribble/lp/tasmod/savestates/exceptions/SavestateException.java
+++ src/main/java/de/scribble/lp/tasmod/savestates/exceptions/SavestateException.java
@@ -1,4 +1,4 @@
-package de.scribble.lp.tasmod.misc;
+package de.scribble.lp.tasmod.savestates.exceptions;
 
 public class SavestateException extends Exception{
 	public SavestateException(String s) {
