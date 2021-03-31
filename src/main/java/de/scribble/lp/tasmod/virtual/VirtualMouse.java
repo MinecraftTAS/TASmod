1b749110504aa7c8f0d1a73105a5a0b7ca57e060
Scribble <ScribbleLP@users.noreply.github.com>
2021-03-31 23:06:54 +0200

    [WIP] Doing the thing

--- src/main/java/de/scribble/lp/tasmod/virtual/VirtualMouse.java
+++ src/main/java/de/scribble/lp/tasmod/virtual/VirtualMouse.java
@@ -2,74 +2,120 @@ package de.scribble.lp.tasmod.virtual;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.google.common.collect.Maps;
 
 public class VirtualMouse {
-	
-	private final Map<Integer, VirtualKey> keyList = Maps.<Integer, VirtualKey>newHashMap();
 
-	public VirtualMouse() {
-		keyList.put(-101,new VirtualKey("MOUSEMOVED", -101));
-		keyList.put(-100,new VirtualKey("LC", -100));
-		keyList.put(-99,new VirtualKey("RC", -99));
-		keyList.put(-98,new VirtualKey("MC", -98));
-		keyList.put(-97,new VirtualKey("MBUTTON3", -97));
-		keyList.put(-96,new VirtualKey("MBUTTON4", -96));
-		keyList.put(-95,new VirtualKey("MBUTTON5", -95));
-		keyList.put(-94,new VirtualKey("MBUTTON6", -94));
-		keyList.put(-93,new VirtualKey("MBUTTON7", -93));
-		keyList.put(-92,new VirtualKey("MBUTTON8", -92));
-		keyList.put(-91,new VirtualKey("MBUTTON9", -91));
-		keyList.put(-90,new VirtualKey("MBUTTON10", -90));
-		keyList.put(-89,new VirtualKey("MBUTTON11", -89));
-		keyList.put(-88,new VirtualKey("MBUTTON12", -88));
-		keyList.put(-87,new VirtualKey("MBUTTON13", -87));
-		keyList.put(-86,new VirtualKey("MBUTTON14", -86));
-		keyList.put(-85,new VirtualKey("MBUTTON15", -85));
+	private Map<Integer, VirtualKey> keyList = Maps.<Integer, VirtualKey>newHashMap();
+
+	private float camX;
+
+	private float camY;
+
+	private int cursorX;
+
+	private int cursorY;
+
+	public VirtualMouse(Map<Integer, VirtualKey> keyListIn) {
+		Map<Integer, VirtualKey> copy = new HashMap<Integer, VirtualKey>();
+
+		keyListIn.forEach((key, value) -> {
+			copy.put(key, value.clone());
+		});
+		keyList = copy;
 	}
-	
+
+	/**
+	 * Creates a Keyboard, where the keys are all unpressed
+	 */
+	public VirtualMouse() {
+		keyList.put(-101, new VirtualKey("MOUSEMOVED", -101));
+		keyList.put(-100, new VirtualKey("LC", -100));
+		keyList.put(-99, new VirtualKey("RC", -99));
+		keyList.put(-98, new VirtualKey("MC", -98));
+		keyList.put(-97, new VirtualKey("MBUTTON3", -97));
+		keyList.put(-96, new VirtualKey("MBUTTON4", -96));
+		keyList.put(-95, new VirtualKey("MBUTTON5", -95));
+		keyList.put(-94, new VirtualKey("MBUTTON6", -94));
+		keyList.put(-93, new VirtualKey("MBUTTON7", -93));
+		keyList.put(-92, new VirtualKey("MBUTTON8", -92));
+		keyList.put(-91, new VirtualKey("MBUTTON9", -91));
+		keyList.put(-90, new VirtualKey("MBUTTON10", -90));
+		keyList.put(-89, new VirtualKey("MBUTTON11", -89));
+		keyList.put(-88, new VirtualKey("MBUTTON12", -88));
+		keyList.put(-87, new VirtualKey("MBUTTON13", -87));
+		keyList.put(-86, new VirtualKey("MBUTTON14", -86));
+		keyList.put(-85, new VirtualKey("MBUTTON15", -85));
+	}
+
 	public void add(int keycode) {
-		String keyString= Integer.toString(keycode);
-		
+		String keyString = Integer.toString(keycode);
+
 		keyList.put(keycode, new VirtualKey(keyString, keycode));
 	}
-	
+
 	public VirtualKey get(int keycode) {
 		return keyList.get(keycode);
 	}
-	
+
 	public VirtualKey get(String keyname) {
-		Collection<VirtualKey> list=keyList.values();
-		VirtualKey out=null;
-		
-		for (VirtualKey key: list) {
-			if(key.getName().equalsIgnoreCase(keyname)) {
-				out=key;
+		Collection<VirtualKey> list = keyList.values();
+		VirtualKey out = null;
+
+		for (VirtualKey key : list) {
+			if (key.getName().equalsIgnoreCase(keyname)) {
+				out = key;
 			}
 		}
 		return out;
 	}
-	
-	public void unpressEverything() {
-		keyList.forEach((key,virtual)->{
-			virtual.setPressed(false);
-		});
-	}
-	
-	public List<String> getCurrentPresses(){
-		List<String> out=new ArrayList<String>();
-		
-		keyList.forEach((keycodes, virtualkeys)->{
-			if(keycodes>=0) {
-				if(virtualkeys.isKeyDown()) {
+
+	public List<String> getCurrentPresses() {
+		List<String> out = new ArrayList<String>();
+
+		keyList.forEach((keycodes, virtualkeys) -> {
+			if (keycodes >= 0) {
+				if (virtualkeys.isKeyDown()) {
 					out.add(virtualkeys.getName());
 				}
 			}
 		});
-		
+
 		return out;
+	}
+
+	public Map<Integer, VirtualKey> getKeyList() {
+		return this.keyList;
+	}
+
+	public void setCam(float x, float y) {
+		camX = x;
+		camY = y;
+	}
+
+	public void setCursor(int x, int y) {
+		cursorX = x;
+		cursorY = y;
+	}
+
+	public List<VirtualMouseEvent> getDifference(VirtualMouse mouseToCompare) {
+		List<VirtualMouseEvent> eventList= new ArrayList<VirtualMouseEvent>();
+		
+		keyList.forEach((keycodes, virtualkeys)->{
+			VirtualKey keyToCompare=mouseToCompare.get(keycodes);
+			if(!virtualkeys.equals(keyToCompare)) {
+//				eventList.add(new VirtualMouseEvent(keycodes, keyToCompare.isKeyDown(), scrollwheel, mouseX, mouseY, slotidx))
+			}
+		});
+		return eventList;
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		return new VirtualMouse(keyList);
 	}
 }
--- src/main/java/de/scribble/lp/tasmod/virtual/VirtualMouseEvent.java
+++ src/main/java/de/scribble/lp/tasmod/virtual/VirtualMouseEvent.java
@@ -2,6 +2,7 @@ package de.scribble.lp.tasmod.virtual;
 
 /**
  * Template for recording Mouse.next() events.
+ * 
  * @author ScribbleLP
  *
  */
@@ -11,33 +12,32 @@ public class VirtualMouseEvent {
 	private int scrollwheel;
 	private int mouseX;
 	private int mouseY;
-	private int slotidx;
-	
-	
-	public VirtualMouseEvent(int keycode, boolean state, int scrollwheel, int mouseX, int mouseY, int slotidx) {
-		this.keyCode=keycode;
-		this.state=state;
-		this.scrollwheel=scrollwheel;
-		this.mouseX=mouseX;
-		this.mouseY=mouseY;
-		this.slotidx=slotidx;
+
+	public VirtualMouseEvent(int keycode, boolean state, int scrollwheel, int mouseX, int mouseY) {
+		this.keyCode = keycode;
+		this.state = state;
+		this.scrollwheel = scrollwheel;
+		this.mouseX = mouseX;
+		this.mouseY = mouseY;
 	}
+
 	public int getKeyCode() {
 		return keyCode;
 	}
+
 	public boolean isState() {
 		return state;
 	}
+
 	public int getScrollwheel() {
 		return scrollwheel;
 	}
+
 	public int getMouseX() {
 		return mouseX;
 	}
+
 	public int getMouseY() {
 		return mouseY;
-	}
-	public int getSlotidx() {
-		return slotidx;
 	}
 }
