685a56ad85b95fba42d8538a95a184b6d40bdf0b
Pfannekuchen <gamesupport@mgnet.work>
2021-04-06 17:49:00 +0200

    Sign Jar. Note: run build instead of shadowJar

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -5,6 +5,6 @@ public class Test {
 	// Pancake asks: Why does an empty file require 5 imports?
 	// Scribble answers: Woosh, they are gone
 	public static void main(String[] args) {
-		//My code, my rules...
+		//No code, no rules...
 	}
 }
