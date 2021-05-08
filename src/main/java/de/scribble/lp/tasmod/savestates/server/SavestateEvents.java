36e201f78ac56a43c38df01b1b546ecb1eb50d29
Scribble <ScribbleLP@users.noreply.github.com>
2021-05-08 15:46:38 +0200

    [WIP] Lag prevention?

--- src/main/java/de/scribble/lp/tasmod/savestates/server/SavestateEvents.java
+++ src/main/java/de/scribble/lp/tasmod/savestates/server/SavestateEvents.java
@@ -13,6 +13,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 public class SavestateEvents {
+	public static boolean lagServer;
 	@SubscribeEvent
 	public void onRender(TickEvent.RenderTickEvent ev) {
 		if(VirtualKeybindings.isKeyDownExceptTextfield(ClientProxy.savestateSaveKey)){
@@ -24,7 +25,7 @@ public class SavestateEvents {
 			CommonProxy.NETWORK.sendToServer(new LoadstatePacket());
 			
 		}else if(VirtualKeybindings.isKeyDownExceptTextfield(ClientProxy.testingKey)) {
-			
+			lagServer=true;
 		}else if(VirtualKeybindings.isKeyDownExceptTextfield(ClientProxy.infoGuiKey)) { // Sorry.. 
 			Minecraft.getMinecraft().displayGuiScreen(new SettingsGui());
 		}else if(VirtualKeybindings.isKeyDown(ClientProxy.stopkey)) {
