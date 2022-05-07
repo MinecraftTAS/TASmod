9cb5f670026db98b762298aad3c1fa592f8666b5
Scribble <ScribbleLP@users.noreply.github.com>
2022-05-07 22:19:42 +0200

    Changes to input loading in ClientSavestates
    - Loading a Client Savestate that doesn't exist prompted the container to clear all inputs. This is now removed.
    - If nothing is playing, inputs won't be overriden by the client savestates anmore, only when recording or playing back

--- src/main/java/de/scribble/lp/tasmod/savestates/client/InputSavestatesHandler.java
+++ src/main/java/de/scribble/lp/tasmod/savestates/client/InputSavestatesHandler.java
@@ -3,10 +3,16 @@ package de.scribble.lp.tasmod.savestates.client;
 import java.io.File;
 import java.io.IOException;
 
+import com.mojang.realmsclient.gui.ChatFormatting;
+
 import de.scribble.lp.tasmod.ClientProxy;
 import de.scribble.lp.tasmod.CommonProxy;
+import de.scribble.lp.tasmod.TASmod;
 import de.scribble.lp.tasmod.inputcontainer.InputContainer;
 import de.scribble.lp.tasmod.savestates.server.exceptions.SavestateException;
+import de.scribble.lp.tasmod.util.TASstate;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -43,9 +49,9 @@ public class InputSavestatesHandler {
 
 		InputContainer container = ClientProxy.virtual.getContainer();
 		if (container.isRecording()) {
-			ClientProxy.serialiser.saveToFileV1(targetfile, container);
+			ClientProxy.serialiser.saveToFileV1(targetfile, container);	//If the container is recording, store it entirely
 		} else if(container.isPlayingback()){
-			ClientProxy.serialiser.saveToFileV1Until(targetfile, container, container.index());
+			ClientProxy.serialiser.saveToFileV1Until(targetfile, container, container.index()); //If the container is playing, store it until the current index
 		}
 	}
 
@@ -68,10 +74,16 @@ public class InputSavestatesHandler {
 
 		File targetfile = new File(savestateDirectory, nameOfSavestate + ".tas");
 
-		if (!targetfile.exists()) {
-			ClientProxy.virtual.getContainer().clear();
-		} else {
-			ClientProxy.virtual.loadSavestate(ClientProxy.serialiser.fromEntireFileV1(targetfile));
+		InputContainer container = ClientProxy.virtual.getContainer();
+		if (!container.isNothingPlaying()) { // If the file exists and the container is recording or playing, load the clientSavestate
+			if (targetfile.exists()) {
+				ClientProxy.virtual.loadClientSavestate(ClientProxy.serialiser.fromEntireFileV1(targetfile));
+			} else {
+				ClientProxy.virtual.getContainer().setTASState(TASstate.NONE, false);
+				Minecraft.getMinecraft().player.sendMessage(new TextComponentString(ChatFormatting.YELLOW
+						+ "Inputs could not be loaded for this savestate, since the file doesn't exist. Stopping!"));
+				TASmod.logger.warn("Inputs could not be loaded for this savestate, since the file doesn't exist.");
+			}
 		}
 	}
 }
