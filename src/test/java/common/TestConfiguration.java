e756dc021bc1197642ff40b151b79b6ed8ac9f07
Scribble <scribble@minecrafttas.com>
2023-05-22 13:34:34 +0200

    Add unit testing

---
+++ src/test/java/common/TestConfiguration.java
@@ -0,0 +1,72 @@
+package common;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import com.minecrafttas.common.Configuration;
+import com.minecrafttas.common.Configuration.ConfigOptions;
+
+class TestConfiguration {
+
+	private static Configuration config;
+	
+	private static final File configPath = new File("./src/test/resources/config.xml");
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		config = new Configuration("Test config", configPath);
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		configPath.delete();
+	}
+
+	@Test
+	void testIfInitialized() {
+		assertNotNull(config);
+	}
+	
+	@Test
+	void testDefault() {
+		configPath.delete();
+		config = new Configuration("Test config", configPath);
+		assertEquals("", config.get(ConfigOptions.FileToOpen));
+	}
+	
+	@Test
+	void testSavingAndLoading() {
+		config.set(ConfigOptions.FileToOpen, "Test");
+		config = new Configuration("Test config", configPath);
+		assertEquals("Test", config.get(ConfigOptions.FileToOpen));
+	}
+	
+	@Test
+	void testIntegers() {
+		config.set(ConfigOptions.FileToOpen, 3);
+		assertEquals(3, config.getInt(ConfigOptions.FileToOpen));
+	}
+
+	@Test
+	void testBooleans() {
+		config.set(ConfigOptions.FileToOpen, true);
+		assertEquals(true, config.getBoolean(ConfigOptions.FileToOpen));
+	}
+	
+	@Test
+	void testDeleteAndContains() {
+		config.delete(ConfigOptions.FileToOpen);
+		assertFalse(config.has(ConfigOptions.FileToOpen));
+	}
+	
+	@Test
+	void resetToDefault() {
+		config.reset(ConfigOptions.FileToOpen);
+		assertEquals("", config.get(ConfigOptions.FileToOpen));
+	}
+}
