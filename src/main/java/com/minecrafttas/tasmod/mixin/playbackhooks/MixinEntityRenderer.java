5935e4a1b7220780308c57cff0d70e35231aa356
Scribble <scribble@minecrafttas.com>
2024-02-16 09:29:45 +0100

    [VirtualInput] Added documentation for MixinEntityRenderer

--- src/main/java/com/minecrafttas/tasmod/mixin/playbackhooks/MixinEntityRenderer.java
+++ src/main/java/com/minecrafttas/tasmod/mixin/playbackhooks/MixinEntityRenderer.java
@@ -45,13 +45,20 @@ public class MixinEntityRenderer implements SubtickDuck {
 	@Shadow
 	private float smoothCamFilterY;
 
+	/**
+	 * Injects into the vanilla camera updating cycle, runs every frame.
+	 * Updates {@link com.minecrafttas.tasmod.virtual.VirtualInput.VirtualCameraAngleInput#nextCameraAngle}
+	 * @param partialTicks The partial ticks of the timer, unused
+	 * @param nanoTime The nanoTime, unused
+	 * @param ci CBI
+	 */
 	@Inject(method = "updateCameraAndRender", at = @At(value = "INVOKE", target = "Lnet/minecraft/profiler/Profiler;startSection(Ljava/lang/String;)V", ordinal = 0, shift = At.Shift.AFTER))
 	public void playback_injectAtStartSection(float partialTicks, long nanoTime, CallbackInfo ci) {
 		// Calculate sensitivity
 		float mouseSensititvity = this.mc.gameSettings.mouseSensitivity * 0.6F + 0.2F;
 		float mouseSensitivityCubed = mouseSensititvity * mouseSensititvity * mouseSensititvity * 8.0F;
 
-		if (this.mc.currentScreen == null && !TASmodClient.controller.isPlayingback() && mc.player != null) { // No Gui
+		if (this.mc.currentScreen == null && !TASmodClient.controller.isPlayingback() && mc.player != null) {
 			mc.mouseHelper.mouseXYChange();
 			float deltaPitch = mc.mouseHelper.deltaY * mouseSensitivityCubed;
 			float deltaYaw = mc.mouseHelper.deltaX * mouseSensitivityCubed;
@@ -64,10 +71,10 @@ public class MixinEntityRenderer implements SubtickDuck {
 			if (this.mc.gameSettings.smoothCamera) {
 				this.smoothCamPitch += deltaPitch;
 				this.smoothCamYaw += deltaYaw;
-				float m = mouseSensititvity - this.smoothCamPartialTicks;
+				float partialSensitivity = mouseSensititvity - this.smoothCamPartialTicks;
 				this.smoothCamPartialTicks = mouseSensititvity;
-				deltaPitch = this.smoothCamFilterY * m;
-				deltaYaw = this.smoothCamFilterX * m;
+				deltaPitch = this.smoothCamFilterY * partialSensitivity;
+				deltaYaw = this.smoothCamFilterX * partialSensitivity;
 			} else {
 				this.smoothCamYaw = 0.0F;
 				this.smoothCamPitch = 0.0F;
@@ -85,49 +92,93 @@ public class MixinEntityRenderer implements SubtickDuck {
 		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 * Runs every tick
+	 * @see VirtualInput.VirtualCameraAngleInput#nextCameraTick()
+	 * @param partialTicks The partial ticks from the vanilla Minecraft timer
+	 */
 	@Override
 	public void runUpdate(float partialTicks) {
 			if(mc.player == null){
 				return;
 			}
+			// Update the currentCameraAngle
 			TASmodClient.virtual.CAMERA_ANGLE.nextCameraTick();
-			
+
+			// Store current rotation to be used as prevRotationPitch/Yaw
 			float prevPitch = mc.player.rotationPitch;
 			float prevYaw = mc.player.rotationYaw;
+
+			// Get the new pitch from the virtual input
 			Float newPitch = TASmodClient.virtual.CAMERA_ANGLE.getCurrentPitch();
 			Float newYaw = TASmodClient.virtual.CAMERA_ANGLE.getCurrentYaw();
-			
-			if(newPitch == null) {
+
+			// If the pitch or yaw is null (usually on initialize or when the player joins the world),
+			// set nextCameraAngle to the current absolute camera coordinates.
+			// This ensures that the camera position is loaded correctly
+			if(newPitch == null || newYaw == null) {
 				TASmodClient.virtual.CAMERA_ANGLE.setCamera(prevPitch, prevYaw);
 				return;
 			}
+
+			// Update the rotation of the player
 			mc.player.rotationPitch = newPitch;
 			mc.player.rotationYaw = newYaw;
 
+			// Update the previous rotation of the player
 			mc.player.prevRotationPitch = prevPitch;
 			mc.player.prevRotationYaw = prevYaw;
 	}
-    
+
+	/**
+	 * Redirects applying the pitch to the camera.
+	 * @param pitch Original pitch of the camera
+	 * @param sharedPitch MixinExtras parameter for sharing values between mixins
+	 * @return 0f for disabeling this method
+	 */
 	@ModifyArg(method = "orientCamera", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;rotate(FFFF)V", ordinal = 8), index = 0)
 	public float redirect_orientCameraPitch(float pitch, @Share("pitch") LocalFloatRef sharedPitch) {
 		sharedPitch.set(pitch);
 		return 0f;
 	}
-	
+
+	/**
+	 * Redirects applying the yaw to the animal camera
+	 * @param yawAnimal Original yaw of the animal camera
+	 * @param sharedPitch MixinExtras parameter for sharing values between mixins
+	 * @return The redirected yaw
+	 */
 	@ModifyArg(method = "orientCamera", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;rotate(FFFF)V", ordinal = 9), index = 0)
 	public float redirect_orientCameraYawAnimal(float yawAnimal, @Share("pitch") LocalFloatRef sharedPitch) {
-		return redirectCam(yawAnimal, sharedPitch.get());
+		return redirectCam(sharedPitch.get(), yawAnimal);
 	}
-	
+
+	/**
+	 * Redirects applying the yaw to the camera
+	 * @param yaw Original yaw of the camera
+	 * @param sharedPitch MixinExtras parameter for sharing values between mixins
+	 * @return The redirected yaw
+	 */
 	@ModifyArg(method = "orientCamera", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/GlStateManager;rotate(FFFF)V", ordinal = 10), index = 0)
 	public float redirect_orientCameraYaw(float yaw, @Share("pitch") LocalFloatRef sharedPitch) {
-		return redirectCam(yaw, sharedPitch.get());
+		return redirectCam(sharedPitch.get(), yaw);
 	}
-	
-	private float redirectCam(float yaw, float pitch) {
+
+	/**
+	 * Turns the camera via GLStateManager
+	 * @param pitch The pi
+	 * @param yaw The yaw
+	 * @see com.minecrafttas.tasmod.virtual.VirtualInput.VirtualCameraAngleInput#getInterpolatedState(float, float, float, boolean)
+	 * @return The redirected yaw
+	 */
+	private float redirectCam(float pitch, float yaw) {
 		Triple<Float, Float, Float> interpolated = TASmodClient.virtual.CAMERA_ANGLE.getInterpolatedState(Minecraft.getMinecraft().timer.renderPartialTicks, pitch, yaw, TASmodClient.controller.isPlayingback());
+		// Update pitch
 		GlStateManager.rotate(interpolated.getLeft(), 1.0f, 0.0f, 0.0f);
+		// Update roll
 		GlStateManager.rotate(interpolated.getRight(), 0.0f, 0.0f, 1.0f);
+		// Update yaw
 		return interpolated.getMiddle();
 	}
 }
